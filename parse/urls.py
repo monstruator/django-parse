f1e9635980c1f4c1683494af5068b86cbcdbb94d
monstruator <pavel212000@gmail.com>
2024-10-31 20:40:49 +0100

    123456

--- parse/urls.py
+++ parse/urls.py
@@ -3,8 +3,10 @@ from .views import *
 from django.contrib.auth import views as auth_views
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib import admin
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', index, name='index'),
 ]
 # + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
