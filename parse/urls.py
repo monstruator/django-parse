b9d17b15d4895a4c7ecf9bc689ee18717fada9d2
monstruator <pavel212000@gmail.com>
2024-10-31 20:36:39 +0100

    1234

---
+++ parse/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import *
+from django.contrib.auth import views as auth_views
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = [
+    path('', index, name='index'),
+
+]
+# + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
